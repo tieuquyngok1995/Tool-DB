7326ef69f1152af8fa675df77b30494a700986e1
tieuquyngok1995 <tieuquyngok1995@gmail.com>
2020-11-21 12:10:36 +0700

    coomit src

---
+++ Tool-DB/View/Main.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace Tool_DB.View
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.txtStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnInputSQL = new System.Windows.Forms.Button();
+            this.btnInputParam = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SQL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.settingToolStripMenuItem.Text = "Setting";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.txtStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 586);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.Stretch = false;
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Margin = new System.Windows.Forms.Padding(670, 3, 0, 2);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(45, 17);
+            this.txtStatus.Text = "Status: ";
+            // 
+            // btnInputSQL
+            // 
+            this.btnInputSQL.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.btnInputSQL.Location = new System.Drawing.Point(524, 547);
+            this.btnInputSQL.Name = "btnInputSQL";
+            this.btnInputSQL.Size = new System.Drawing.Size(103, 27);
+            this.btnInputSQL.TabIndex = 4;
+            this.btnInputSQL.Text = "Input SQL";
+            this.btnInputSQL.UseVisualStyleBackColor = true;
+            // 
+            // btnInputParam
+            // 
+            this.btnInputParam.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.btnInputParam.Location = new System.Drawing.Point(637, 547);
+            this.btnInputParam.Name = "btnInputParam";
+            this.btnInputParam.Size = new System.Drawing.Size(135, 27);
+            this.btnInputParam.TabIndex = 5;
+            this.btnInputParam.Text = "Input Parameter";
+            this.btnInputParam.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.SQL});
+            this.dataGridView.Location = new System.Drawing.Point(14, 30);
+            this.dataGridView.MultiSelect = false;
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.RowTemplate.Height = 21;
+            this.dataGridView.RowTemplate.ReadOnly = true;
+            this.dataGridView.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView.Size = new System.Drawing.Size(759, 505);
+            this.dataGridView.TabIndex = 6;
+            this.dataGridView.Visible = false;
+            // 
+            // ID
+            // 
+            this.ID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.ID.DataPropertyName = "ID";
+            this.ID.Frozen = true;
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.ID.Width = 22;
+            // 
+            // SQL
+            // 
+            this.SQL.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.SQL.DataPropertyName = "SQL";
+            this.SQL.HeaderText = "SQL";
+            this.SQL.Name = "SQL";
+            this.SQL.ReadOnly = true;
+            this.SQL.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.SQL.Width = 32;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 608);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnInputParam);
+            this.Controls.Add(this.btnInputSQL);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "Tool Compare SQL";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel txtStatus;
+        private System.Windows.Forms.Button btnInputSQL;
+        private System.Windows.Forms.Button btnInputParam;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SQL;
+    }
+}
---
+++ Tool-DB/View/Main.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using Tool_DB.View;
+using Tool_DB.Utils;
+using System.Configuration;
+
+namespace Tool_DB.View
+{
+    public partial class Main : Form
+    {
+        Connection con;
+        SqlConnection sqlConnection;
+
+        public string textRowSelect = string.Empty;
+        public bool statusConnection = false;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void settingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Setting frmSetting = new Setting();
+
+            frmSetting.ShowDialog();
+            frmSetting.Dispose();
+
+            if (frmSetting.DialogResult == DialogResult.OK)
+            {
+                sqlConnection = frmSetting.sqlConnection;
+                if (sqlConnection != null && sqlConnection.State.Equals(System.Data.ConnectionState.Open))
+                {
+                    statusConnection = true;
+                    txtStatus.Text = string.Format(CONSTANTS.CONST_STRING_STATUS, CONSTANTS.CONST_STRING_STATUS_CONNECTION);
+                }
+                else
+                {
+                    statusConnection = false;
+                    txtStatus.Text = string.Format(CONSTANTS.CONST_STRING_STATUS, CONSTANTS.CONST_STRING_STATUS_DISCONNECT);
+                }
+                btnInputSQL.Enabled = statusConnection;
+                btnInputParam.Enabled = statusConnection;
+            }
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            txtStatus.Text = CheckIsConnection()
+                ? string.Format(CONSTANTS.CONST_STRING_STATUS, CONSTANTS.CONST_STRING_STATUS_CONNECTION)
+                : string.Format(CONSTANTS.CONST_STRING_STATUS, CONSTANTS.CONST_STRING_STATUS_DISCONNECT);
+
+            btnInputSQL.Enabled = statusConnection;
+            btnInputParam.Enabled = statusConnection;
+
+            if (dataGridView.Rows.Count == 0)
+            {
+                btnInputParam.Enabled = false;
+            }
+        }
+
+        private bool CheckIsConnection()
+        {
+            string connectionString = string.Format("Data Source={0};Initial Catalog={1};User ID={2};Password={3};",
+               ConfigurationManager.AppSettings[CONSTANTS.CONST_STRING_SERVER_NAME],
+               ConfigurationManager.AppSettings[CONSTANTS.CONST_STRING_DATABASE_NAME],
+               ConfigurationManager.AppSettings[CONSTANTS.CONST_STRING_USER_NAME],
+               ConfigurationManager.AppSettings[CONSTANTS.CONST_STRING_PASSWORD]);
+
+            try
+            {
+                con = new Connection(connectionString);
+                if (con.IsConnection)
+                {
+                    statusConnection = true;
+                    return true;
+                }
+
+            }
+            catch (Exception)
+            {
+            }
+            statusConnection = false;
+            return false;
+        }
+
+        private void Main_Shown(object sender, EventArgs e)
+        {
+            if (statusConnection) return;
+
+            Setting frmSetting = new Setting();
+
+            frmSetting.ShowDialog();
+            frmSetting.Dispose();
+
+            if (frmSetting.DialogResult == DialogResult.OK)
+            {
+                sqlConnection = frmSetting.sqlConnection;
+                if (sqlConnection != null && sqlConnection.State.Equals(System.Data.ConnectionState.Open))
+                {
+                    statusConnection = true;
+                    txtStatus.Text = string.Format(CONSTANTS.CONST_STRING_STATUS, CONSTANTS.CONST_STRING_STATUS_CONNECTION);
+                }
+                else
+                {
+                    statusConnection = false;
+                    txtStatus.Text = string.Format(CONSTANTS.CONST_STRING_STATUS, CONSTANTS.CONST_STRING_STATUS_DISCONNECT);
+                }
+                btnInputSQL.Enabled = statusConnection;
+                btnInputParam.Enabled = statusConnection;
+            }
+        }
+
+        private void btnInputSQL_Click(object sender, EventArgs e)
+        {
+            InputSQL frmInputSQL = new InputSQL();
+
+            frmInputSQL.ShowDialog();
+            frmInputSQL.Dispose();
+
+            if (frmInputSQL.DialogResult == DialogResult.OK)
+            {
+                dataGridView.DataSource = frmInputSQL.data;
+
+                if (dataGridView.Rows.Count == 0)
+                {
+                    dataGridView.Visible = false;
+                    btnInputParam.Enabled = false;
+                }
+                else
+                {
+                    dataGridView.Visible = true;
+                    btnInputParam.Enabled = true;
+                }
+            }
+        }
+
+        private void btnInputParam_Click(object sender, EventArgs e)
+        {
+            int selectedrowindex = dataGridView.SelectedCells[0].RowIndex;
+            DataGridViewRow selectedRow = dataGridView.Rows[selectedrowindex];
+            textRowSelect = selectedRow.Cells[CONSTANTS.CONST_STRING_COLUMNS_SQL].Value.ToString();
+            InputParam frmInputParam = new InputParam(textRowSelect);
+            frmInputParam.ShowDialog();
+            frmInputParam.Dispose();
+        }
+    }
+}
